4b69ed0c4a3ca1b992297e6c1f7944f741c4d83f
VirtusProVM <92260039+VirtusProVM@users.noreply.github.com>
2021-10-16 18:08:26 +0200

    Update paypalConfig.php

--- files/paypalConfig.php
+++ files/paypalConfig.php
@@ -4,8 +4,8 @@ require_once("PayPal-PHP-SDK/autoload.php");
 
 $apiContext = new \PayPal\Rest\ApiContext(
 	new \PayPal\Auth\OAuthTokenCredential(
-		'AUeJpUrFlnc0nq2dG3pUQ14qzJ2mRZ3qtBNNbBkY7wrZxO2yCpCaaBvtpokILApu4R0t7iBGkmuYsGGn', 
-		'EP10pJA8zv7eYJd4zcxev1PkVB6dt5Px2V8vOdzCPOYQG63X-oQ0S8MzyHM9FsDO5Pclorct5-WJtUNp'
+		'AUeJpUrFlnc0nq2dG3pUQ14qzJ2mRZ3qtBNNbBkY7wrZxO2yCpCaaBvtpokILApu4R0t7iBGkmuhvgvjbknlkj', 
+		'EP10pJA8zv7eYJd4zcxev1PkVB6dt5Px2V8vOdzCPOYQG63X-oQ0S8MzyHM9FsDO5Pclorct5-WJtUNpjhvbnj'
 	)
 );
 ?>
